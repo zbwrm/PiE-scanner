0e2237105bd2cf1e8c0ab8bc4f4c621ae9ea996e
Aydin O'Leary <aoleary@olin.edu>
2021-09-21 10:53:48 -0400

    Add conversion to Cartesian coords

--- data_processing.py
+++ data_processing.py
@@ -5,8 +5,10 @@ import argparse, logging
 
 logging.info('creating global variables...')
 HERE = os.path.dirname(os.path.abspath(__file__))
-H_RESOLUTION = 100
-V_RESOLUTION = 100
+H_RESOLUTION  = 100       # how many horizontal samples
+V_RESOLUTION  = 100       # how many vertical samples
+H_ANGLE_RANGE = [-20, 20] # horizontal angular range of motion (in degrees)
+V_ANGLE_RANGE = [-30, 30] # vertical range of motion (in degrees)
 
 logging.info('initializing...')
 start_time = datetime.now()
@@ -14,6 +16,7 @@ parser = argparse.ArgumentParser(description='3D Scanner Interface Program')
 parser.add_argument('filename', type=str,
                     help='Names the CSV that is created. ([filename].csv)')
 args = parser.parse_args()
+
 device = "/dev/cu.usbmodem14101"
 baud   = "9600"
 
@@ -26,29 +29,31 @@ file = open(filename, 'w')
 file.close()
 
 logging.info('generating horizontal angles...')
-u_angles = []
-for i in range(H_RESOLUTION):
-    pass
-logging.info('generating vertical angles...')
+u_angles = np.linrange(H_ANGLE_RANGE[0], H_ANGLE_RANGE[1], H_RESOLUTION)
+# for i in range(H_RESOLUTION):
+#     pass
 
-# generate v angles
-v_angles = []
-for i in range(V_RESOLUTION):
-    pass
+logging.info('generating vertical angles...')
+v_angles = np.linrange(V_ANGLE_RANGE[0], V_ANGLE_RANGE[1], V_RESOLUTION)
+# for i in range(V_RESOLUTION):
+#     pass
 
 
 # take data
-out_array = np.zeros((u, v))
+out_array = np.array()
 with serial.Serial(device,baud) as ser:
     for u in u_angles:
         for v in v_angles:
             logging.debug(f'ordering arduino to rotate to {round(u,2)} and {round(v,2)}...')
             ser.write(bytes(str(u) + " " + str(v)))
             time.sleep(0.025)
-            logging.debug(f'accepting averaged measurement from arduino...')
-            in_data = ser.readline()
-            logging.info(f'measuring {round(in_data,2)} at {round(u,2)} and {round(v,2)}')
-            out_array[u, v] = in_data
+            logging.debug('accepting averaged measurement from arduino...')
+            d = int(ser.readline())
+            logging.info(f'measuring {round(d,2)} at {round(u,2)} and {round(v,2)}')
+            x = d * np.cos(v) * np.sin(u)
+            y = d * np.cos(v) * np.cos(u)
+            z = d * np.sin(v)
+            out_array.append(f'{x},{y},{z}\n')
     # while 1:
     #     line = serial_port.readline()
 

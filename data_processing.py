d92a7816df49026c675e2590b824a0bcf1bc3a89
Aydin O'Leary <aoleary@olin.edu>
2021-09-23 20:54:35 -0400

    Remember that np.linrange doesn't exist

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,4 @@
-import serial, os
+import serial, struct, os
 from datetime import datetime, time
 import numpy as np
 import argparse, logging
@@ -29,10 +29,10 @@ file = open(filename, 'w')
 file.close()
 
 logging.info('generating horizontal angles...')
-u_angles = np.linrange(H_ANGLE_RANGE[0], H_ANGLE_RANGE[1], H_RESOLUTION)
+u_angles = np.linspace(H_ANGLE_RANGE[0], H_ANGLE_RANGE[1], H_RESOLUTION)
 
 logging.info('generating vertical angles...')
-v_angles = np.linrange(V_ANGLE_RANGE[0], V_ANGLE_RANGE[1], V_RESOLUTION)
+v_angles = np.linspace(V_ANGLE_RANGE[0], V_ANGLE_RANGE[1], V_RESOLUTION)
 
 out_array = np.array()
 logging.info('initializing serial connection...')
@@ -42,7 +42,7 @@ with serial.Serial(device,baud) as ser:
             logging.debug(f'ordering arduino to rotate to {round(u,2)} and {round(v,2)}...')
 
             # this section heavily dependant on 2-way serial communication w/ the arduino. mostly TBD
-            ser.write(bytes(np.float32(u)))
+            ser.write(struct.pack('2f', u, v))
 
 
             time.sleep(0.025)

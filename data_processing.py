402bd0376bec412a4a21e1f695ebf1763849d44d
Aydin O'Leary <aoleary@olin.edu>
2021-09-24 11:01:34 -0400

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,9 @@
-import serial, struct, os
-from datetime import datetime, time
+import serial, struct, os, time
+from datetime import datetime
 import numpy as np
 import argparse, logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 logging.info('creating global variables...')
 HERE = os.path.dirname(os.path.abspath(__file__))
@@ -17,7 +19,7 @@ parser.add_argument('filename', type=str,
                     help='Names the CSV that is created. ([filename].csv)')
 args = parser.parse_args()
 
-device = "/dev/cu.usbmodem14101"
+device = "/dev/cu.usbmodem14301"
 baud   = "9600"
 
 logging.info('creating file to write to...')
@@ -34,9 +36,13 @@ u_angles = np.linspace(H_ANGLE_RANGE[0], H_ANGLE_RANGE[1], H_RESOLUTION)
 logging.info('generating vertical angles...')
 v_angles = np.linspace(V_ANGLE_RANGE[0], V_ANGLE_RANGE[1], V_RESOLUTION)
 
-out_array = np.array()
+out_array = np.array(float)
 logging.info('initializing serial connection...')
+
 with serial.Serial(device,baud) as ser:
+    while (ser.readline().strip() != b'<open>'):
+        continue
+    
     for u in u_angles:
         for v in v_angles:
             logging.debug(f'ordering arduino to rotate to {round(u,2)} and {round(v,2)}...')
@@ -47,11 +53,12 @@ with serial.Serial(device,baud) as ser:
 
             time.sleep(0.025)
             logging.debug('accepting averaged measurement from arduino...')
-            d = int(ser.readline())
-            logging.info(f'measuring {round(d,2)} at {round(u,2)} and {round(v,2)}')
-            x = d * np.cos(v) * np.sin(u)
-            y = d * np.cos(v) * np.cos(u)
-            z = d * np.sin(v)
-            out_array.append(f'{x},{y},{z}\n')
+            d = struct.unpack('f', ser.readline().strip())
+            print(d[0])
+            # logging.info(f'measuring {round(d,2)} at {round(u,2)} and {round(v,2)}')
+            # x = d * np.cos(v) * np.sin(u)
+            # y = d * np.cos(v) * np.cos(u)
+            # z = d * np.sin(v)
+            # out_array.append(f'{x},{y},{z}\n')
 
 np.savetxt(filename, out_array)

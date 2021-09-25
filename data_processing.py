47f4e099620426832ac086eb051ae3c912ca138b
Aydin O'Leary <aoleary@olin.edu>
2021-09-25 17:02:12 -0400

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -7,8 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 logging.info('creating global variables...')
 HERE = os.path.dirname(os.path.abspath(__file__))
-H_RESOLUTION  = 100       # how many horizontal samples
-V_RESOLUTION  = 100       # how many vertical samples
+
 H_ANGLE_RANGE = [-20, 20] # horizontal angular range of motion (in degrees)
 V_ANGLE_RANGE = [-30, 30] # vertical range of motion (in degrees)
 
@@ -19,7 +18,7 @@ parser.add_argument('filename', type=str,
                     help='Names the CSV that is created. ([filename].csv)')
 args = parser.parse_args()
 
-device = "/dev/cu.usbmodem14301"
+device = "/dev/cu.usbmodem14101"
 baud   = "9600"
 
 logging.info('creating file to write to...')
@@ -30,31 +29,26 @@ filename = os.path.join(HERE, f"./scans/{args.filename}.csv")
 file = open(filename, 'w')
 file.close()
 
-logging.info('generating horizontal angles...')
-u_angles = np.linspace(H_ANGLE_RANGE[0], H_ANGLE_RANGE[1], H_RESOLUTION)
-
-logging.info('generating vertical angles...')
-v_angles = np.linspace(V_ANGLE_RANGE[0], V_ANGLE_RANGE[1], V_RESOLUTION)
-
 out_array = np.array(float)
 logging.info('initializing serial connection...')
 
 with serial.Serial(device,baud) as ser:
     while (ser.readline().strip() != b'<open>'):
         continue
-    
-    for u in u_angles:
-        for v in v_angles:
+
+    for u in range(H_ANGLE_RANGE[0],H_ANGLE_RANGE[1]):
+        for v in range(V_ANGLE_RANGE[0],V_ANGLE_RANGE[1]):
             logging.debug(f'ordering arduino to rotate to {round(u,2)} and {round(v,2)}...')
 
             # this section heavily dependant on 2-way serial communication w/ the arduino. mostly TBD
-            ser.write(struct.pack('2f', u, v))
+            # ser.write(struct.pack('2i', u+90, v+90))
+            print(struct.pack('2i', u+90, v+90))
 
 
-            time.sleep(0.025)
-            logging.debug('accepting averaged measurement from arduino...')
-            d = struct.unpack('f', ser.readline().strip())
-            print(d[0])
+            # time.sleep(0.025)
+            # logging.debug('accepting averaged measurement from arduino...')
+            # d = struct.unpack('f', ser.readline().strip())
+            # print(d[0])
             # logging.info(f'measuring {round(d,2)} at {round(u,2)} and {round(v,2)}')
             # x = d * np.cos(v) * np.sin(u)
             # y = d * np.cos(v) * np.cos(u)
